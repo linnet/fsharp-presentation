67be9e02824ad5c09b6217db693d277c743be24e
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2010-02-25 22:17:40 +0100

    Added more examples

--- VSPresentation/02-Tupler.fs
+++ VSPresentation/02-Tupler.fs
@@ -9,7 +9,7 @@ let pair2 = ("Hello", 42)
 
 
 // Tupler kan have et vilkårligt antal elementer
-let pair3 = (1, 5, 7)
+let triple = (1, 5, 7)
 
 
 // Adgang til værdier i tupler
@@ -21,3 +21,12 @@ let second = snd pair4
 // Udpakning vha. pattern matching
 let pair5 = ("Hello", 42)
 let first2, second2 = pair5
+
+
+// Pattern matching med wildcards 
+let triple2 = (1, 5, 7)
+let _, middle, _ = triple2
+
+
+// Tupler som funktionsparametre
+let multTuple (x, y) = x * y
---
+++ VSPresentation/03-Funktioner.fs
@@ -0,0 +1,23 @@
+﻿module Funktioner
+
+// Simpel funktion. Værdien af en funktion er den sidste værdi - ingen eksplicit return
+let mult x y = x * y
+let result = mult 5 2
+
+
+// Funktioner kan nestes
+let outer x =
+    let inner = x * 5
+    inner
+
+let result2 = outer 2
+
+
+
+// Currying - binder funktionsparameter og giver en ny funktion med færre parametre
+let add x y = x + y
+let add10 = add 10
+
+let result3 = add10 5
+
+
