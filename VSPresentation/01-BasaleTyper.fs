f3fbe4e50e89be4c966e37726c5faf8497764f04
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2010-02-25 21:49:21 +0100

    Added a VS2010 solution

---
+++ VSPresentation/01-BasaleTyper.fs
@@ -0,0 +1,17 @@
+﻿module BasaleTyper
+
+// De gængse .NET-typer er understøttet - bemærk brugen af typeinferens
+let tal = 10
+let tal2 = 10.2
+let navn = "Jesper"
+
+
+// Typeinferens på funktioner
+let mult x y = x * y
+
+
+// Mulighed for at hjælpe compileren
+let mult2 (x: float) y = x * y
+
+
+
---
+++ VSPresentation/02-Tupler.fs
@@ -0,0 +1,23 @@
+﻿module Tupler
+
+// Simpel tuple
+let pair1 = (1, 42)
+
+
+// Tupler kan have forskellige typer
+let pair2 = ("Hello", 42)
+
+
+// Tupler kan have et vilkårligt antal elementer
+let pair3 = (1, 5, 7)
+
+
+// Adgang til værdier i tupler
+let pair4 = ("Hello", 42)
+let first = fst pair4
+let second = snd pair4
+
+
+// Udpakning vha. pattern matching
+let pair5 = ("Hello", 42)
+let first2, second2 = pair5
