ce96ae362da22a6965eff99ac27b2ee6f4a65f28
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2010-02-26 22:04:00 +0100

    Added more examples

---
+++ VSPresentation/06-Option.fs
@@ -0,0 +1,16 @@
+﻿module Option
+
+// Option-typen kan opfattes som System.Nullable
+// Giver muligheder i forbindelse med pattern matching
+let maybe x =
+    if x > 5 then
+        Some(x)
+    else
+        None
+
+let result1 = maybe 3
+let result2 = maybe 6
+
+printfn "Result1 is None: %b" result1.IsNone
+printfn "Result2 is None: %b" result2.IsNone
+
