6815783d2bfd8e4b3fc2c35deff3d41de3792547
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2010-02-27 17:22:51 +0100

    Updated examples

---
+++ VSPresentation/10-Records.fs
@@ -0,0 +1,20 @@
+﻿module Records
+
+// ErklXring af en record
+type PersonRec = {
+    First : string;
+    Last : string;
+    Age : int
+}
+
+
+// Brug af en record
+let person = { First = "Jens"; Last = "Hansen"; Age = 38 }
+
+printfn "Fornavn: %s" person.First
+
+
+// Kloning af en record - immutability er godt hjulpet med "with"
+let tvillingebror = { person with First = "Peter" }
+
+
---
+++ VSPresentation/11-LazyEvaluering.fs
@@ -0,0 +1,33 @@
+﻿module LazyEvaluering
+
+// Lazy evaluation bygget ind i sproget med "lazy" keyword'et
+let x = Lazy<int>.Create(fun () -> printfn "Evaluating x..."; 10)
+let y = lazy (printfn "Evaluating y..."; x.Value + x.Value)
+
+let yResult1 = y.Value
+let yResult2 = y.Value
+
+
+
+// Sequences - svarer nogenlunde til lazy lister
+let listOfNumbers = [1..10]
+let seqOfNumbers = seq { 1..10 }
+
+
+let isEven x = x % 2 = 0
+let isSmall x = x < 100
+
+// Listen evalueres med det samme, sekvensen fXrst nXr den bliver brugt
+let evenNumbers = Seq.filter isEven seqOfNumbers
+let evenNumbersAsList = Seq.toList evenNumbers
+
+
+// Lazy evaluering giver muligheder
+
+
+let seqOfAllNumbers = seq { 1..1000000000 }
+let smallEvenNumbers =
+    seqOfAllNumbers
+    |> Seq.filter isSmall
+    |> Seq.filter isEven
+    |> Seq.toList
