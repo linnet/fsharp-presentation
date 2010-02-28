aaa5bede679cc2766337d4fc4d51ff5a584f28e5
Jesper Kamstrup Linnet <jesper@kamstrup-linnet.dk>
2010-02-28 18:17:44 +0100

    Added a C# test client project

---
+++ VSPresentation/15-DotNetIntegrationFraFSharp.fs
@@ -0,0 +1,25 @@
+﻿module DotNetIntegrationFraFSharp
+
+// open svarer til using
+open CSharpCode
+
+let smallNumbers = 
+    // Instantierer og bruger C#-klasse 
+    let csc = new CSharpClass()
+
+    csc.GetNumbers()
+    |> Seq.filter (fun x -> x < 5)
+
+
+// Implementering af extension method
+type System.Int32 with
+    member this.ToHexString() = sprintf "0x%x" this
+
+
+[<EntryPoint>]
+let Main args =
+    smallNumbers
+    |> Seq.iter (printfn "%d")
+    0
+
+
---
+++ VSPresentation/CSharpClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DotN
+namespace CSharpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
